3a6e7989829065d00f6c9284ed16289e9e700560
PresByter <presbyter163@163.com>
2020-03-06 17:39:22 +0800

    mobx over

--- react-ts/src/stores/home.ts
+++ react-ts/src/stores/home.ts
@@ -5,6 +5,12 @@ export default class HomeStore {
 
   @observable name: string = 'Clint'
 
+  @observable token: string = '';
+
+  @observable id: string = '';
+
+  @observable email: string = '';
+
   constructor(initialState: any = { name: 'store' }) {
     this.name = initialState.name;
   }
@@ -14,4 +20,21 @@ export default class HomeStore {
     // console.log(name, this);
     this.name = name
   }
+  async signIn (data: any) {
+    try {
+      // const { data: res } = await api.signIn(data);
+      // this.id = res.data.id;
+      // this.token = res.data.token;
+      // this.email = res.data.email;
+      // this.setLocalStorage({
+      //   id: this.id,
+      //   token: this.token,
+      //   email: this.email
+      // });
+      // return res;
+    } catch (error) {
+      return error;
+    }
+  }
+
 }
