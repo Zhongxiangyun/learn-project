d83128adba01379238e041ceefa4db6f648b97f2
PresByter <presbyter163@163.com>
2020-03-12 18:43:28 +0800

    layout

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import Home from './pages/home/index';
 import Detail from './pages/detail/index';
 import HomeLayout from './components/HomeLayout'
@@ -10,7 +10,7 @@ import NoMatch from './pages/nomatch/index'
 const App: React.FC<{}> = () => {
   return (
     <div className="App">
-      <HomeLayout></HomeLayout>
+      <HomeLayout>
       {/* <HashRouter > */}
         <Switch>
           <Route exact path="/" component={Home} />
@@ -19,6 +19,7 @@ const App: React.FC<{}> = () => {
           <Route path="*" component={NoMatch} />
         </Switch>
       {/* </HashRouter> */}
+      </HomeLayout>
     </div>
   );
 }
--- react-ts/src/components/HomeLayout.tsx
+++ react-ts/src/components/HomeLayout.tsx
@@ -1,18 +1,78 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import { Link } from "react-router-dom";
-const HomeLayout: React.FC<{}> = props => {
-    const {
-        children,
-        // location = {
-        //     pathname: '',
-        // },
-    } = props;
-    return (
-        <div>
-            公共组件
-        {children}
-        </div>
-    )
+import { Layout, Menu } from 'antd';
+import {
+    MenuUnfoldOutlined,
+    MenuFoldOutlined,
+    UserOutlined,
+    VideoCameraOutlined,
+    UploadOutlined,
+} from '@ant-design/icons';
+import './layout.less'
+
+const { Header, Footer, Sider, Content } = Layout;
+interface IProps {
+    // history?:any;
+    // userStore?: userStoreType;
+    errors?: string;
+    [key: string]: any
+}
+
+const initialState = {
+    collapsed: false,
+};
+type State = typeof initialState;
+class HomeLayout extends Component<IProps, State> {
+    state: State = initialState;
+    private toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    };
+    render() {
+        const { children } = this.props;
+
+        return (
+            <Layout className='main'>
+                <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+                    <div className="logo" />
+                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                        <Menu.Item key="1">
+                            <UserOutlined />
+                            <span>nav 1</span>
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            <VideoCameraOutlined />
+                            <span>nav 2</span>
+                        </Menu.Item>
+                        <Menu.Item key="3">
+                            <UploadOutlined />
+                            <span>nav 3</span>
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+                <Layout className="site-layout">
+                    <Header className="site-layout-background" style={{ padding: 0 }}>
+                        {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                            className: 'trigger',
+                            onClick: this.toggle,
+                        })}
+                    </Header>
+                    <Content
+                        className="site-layout-background"
+                        style={{
+                            margin: '24px 16px',
+                            padding: 24,
+                            minHeight: 280,
+                        }}
+                    >
+                        {children}
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+                </Layout>
+            </Layout>
+        )
+    }
 };
 
 export default HomeLayout;
