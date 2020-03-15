1c8808d5c61b189f69fc34d459457084fb68c5f8
PresByter <presbyter163@163.com>
2020-03-15 13:58:53 +0800

    面试 记录

--- es6/test.js
+++ es6/test.js
@@ -20,18 +20,18 @@ outer ();
 // const animal1 = Object.create (Animal);
 // delete animal1.name;
 // console.log (animal1.name);
-
+// 4.
 // (function() {
 //     console.log(1);
 //     setTimeout(function(){console.log(2)}, 1000);
 //     setTimeout(function(){console.log(3)}, 0);
 //     console.log(4);
 // })();
-
+// 5
 // 长轮询、Websockets和服务器发送的事件之间有什么区别？
-
+// 6
 // 简要描述长期缓存以及如何使用webpack实现它
-
+// 7
 // const ary1 = [7, 1, 5, 2, 3, 6];
 // const ary2 = [3, 8, 6, 20, 7];
 
@@ -50,7 +50,7 @@ outer ();
 // input: a[3|bc]de
 // output: abcbcbcde
 /**
- *   Given a string with such a special case `[number|string]`, where `number` indicates the number of repetitions and `string` indicates the string that needs to be repeated. 
+ *  8. Given a string with such a special case `[number|string]`, where `number` indicates the number of repetitions and `string` indicates the string that needs to be repeated. 
  */
 const respeat = str => {
   let number = str.slice (str.search (/\[/g) + 1, str.search (/\|/g));
@@ -60,3 +60,29 @@ const respeat = str => {
 console.log (respeat ('a[3|bc]de'));
 
 respeat ('a[3|bc]de');
+
+/**
+ * 8 道题
+ * 
+ * 1. css
+ * 2. js 闭包
+ * var a = 1;
+function outer () {
+  var a = 2;
+  function inner () {
+    a++;
+    console.log (a);
+    var a = 3;
+    console.log (a);
+  }
+  inner ();
+}
+outer ();
+* 3 对象
+*  const Animal = {
+*   name: 'cat',
+* };
+* const animal1 = Object.create (Animal);
+* delete animal1.name;
+* console.log (animal1.name);
+ */
