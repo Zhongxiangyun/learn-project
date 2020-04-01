c4355977d7b5c9c7ec2fa500b8687eb2b210309a
PresByter <presbyter163@163.com>
2020-04-01 19:35:58 +0800

    jiuye

---
+++ jiuye/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Button type="primary">Primary</Button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import { Button } from "view-design";
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+    Button
+  }
+};
+</script>
