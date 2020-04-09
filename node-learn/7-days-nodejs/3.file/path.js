a248a30787b26e6153be2f4a9ce5804c811267df
PresByter <presbyter163@163.com>
2020-04-09 20:30:04 +0800

    node path

--- node-learn/7-days-nodejs/3.file/path.js
+++ node-learn/7-days-nodejs/3.file/path.js
@@ -1,2 +1,30 @@
 const path = require('path');
+const fs = require('fs');
 console.log(path.normalize);
+
+function travel(dir, callback, finish) {
+    fs.readdir(dir, function (err, files) {
+        (function next(i) {
+            if (i < files.length) {
+                var pathname = path.join(dir, files[i]);
+
+                fs.stat(pathname, function (err, stats) {
+                    if (stats.isDirectory()) {
+                        travel(pathname, callback, function () {
+                            next(i + 1);
+                        });
+                    } else {
+                        callback(pathname, function () {
+                            next(i + 1);
+                        });
+                    }
+                });
+            } else {
+                finish && finish();
+            }
+        }(0));
+    });
+}
+travel('/home/user', function (pathname) {
+    console.log(pathname);
+});
